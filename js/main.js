d2e5b839bb8a3a65f01a813060d345a81ceb7f5f
Glen Hodges <glen.hodges@wiseandhammer.com>
2018-06-15 11:16:04 -0400

    Finished mobile styling update, just need to fiish main hero mobile styling then done

--- js/main.js
+++ js/main.js
@@ -9,115 +9,22 @@ $(document).ready(function() {
 //Open menu when hamburger or menu title is clicked
 //*****************************************************
 
-$(".nav-wrapper").on('click', function(){
-  $(".hamburger").toggleClass('is-active');
-  $("#nav-list").fadeToggle(400);
-});
+var windowWidth = $( window ).width();
+console.log(windowWidth);
 
-
-//*****************************************************
-// AUDIO PLAYER
-//*****************************************************
-
-var audio;
-
-//Hide paude button
-$('#pause').hide();
-
-// Initialize Audio
-initAudio($('#playlist li:first-child'));
-
-// Initilizer function
-function initAudio(element){
-  var title = element.attr('title');
-  var song = element.attr('song');
-
-  // Create audio object
-  audio = new Audio(song);
-
-  if(!audio.currentTime) {
-    $('#duration').html('0.00');
-  }
-
-  $('#audio-player .title').text(title);
-
-  $('#playlist li').removeClass('active');
-  element.addClass('active');
-
-}
-
-//Play button
-$('#play').click(function(){
-  audio.play();
-  $('#play').hide();
-  $('#pause').show();
-  $('#duration').fadeIn(400);
-  showDuration();
-});
-
-// Pause button
-$('#pause').click(function(){
-  audio.pause();
-  $('#pause').hide();
-  $('#play').show();
-});
-
-// Stop button
-$('#stop').click(function(){
-  audio.pause();
-  audio.currentTime = 0;
-  $('#pause').hide();
-  $('#play').show();
-  $('#duration').fadeOut();
-});
-
-// Next button
-$('#next').click(function(){
-  audio.pause();
-  var next = $('#playlist li.active').next();
-  if(next.length === 0) {
-    next = $('#playlist li:first-child');
-  }
-  initAudio(next);
-  audio.play();
-  showDuration();
-});
-
-// Next button
-$('#prev').click(function(){
-  audio.pause();
-  var prev = $('#playlist li.active').prev();
-  if(prev.length === 0) {
-    prev = $('#playlist li:last-child');
-  }
-  initAudio(prev);
-  audio.play();
-  showDuration();
-});
-
-// TIme duration
-function showDuration() {
-  $(audio).bind('timeupdate', function(){
-    var s = parseInt(audio.currentTime % 60);
-    var m = parseInt((audio.currentTime / 60) % 60);
-    // Add 0 if tless than 10
-    if (s > 10) {
-      s = '0' + s;
-    }
-    $('#duration').html(m + '.' + s);
-    var value = 0;
-    if (audio.currentTime > 0) {
-      value = Math.floor((100 / audio.duration) * audio.currentTime);
-    }
-    $('#progress').css('width', value+'%');
+if (windowWidth > 1200) {
+  console.log('greater');
+  $(".nav-wrapper").on('click', function(){
+    $(".hamburger").toggleClass('is-active');
+    $("#nav-list").fadeToggle(400);
+  });
+} else {
+  console.log('less');
+  $(".nav-wrapper").on('click', function(){
+    $(".hamburger").toggleClass('is-active');
+    $("#nav-list").slideToggle(400);
   });
 }
-
-// Volume Controls
-$('#volume').change(function(){
-  audio.volume = parseFloat(this.value / 10);
-});
-
 
 //*****************************************************
 // Fancy Box 
@@ -148,5 +55,6 @@ $("a.grouped_elements").fancybox();
 //*****************************************************
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
 
 }); //END OF DOC READY
